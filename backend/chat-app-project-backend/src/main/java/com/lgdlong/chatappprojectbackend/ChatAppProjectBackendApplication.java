9109aec014fe11041576a8c1a22a3c8639bff006
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-04-21 17:13:55 +0700

    init backend

---
+++ backend/chat-app-project-backend/src/main/java/com/lgdlong/chatappprojectbackend/ChatAppProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lgdlong.chatappprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppProjectBackendApplication.class, args);
+    }
+
+}
---
+++ backend/chat-app-project-backend/src/test/java/com/lgdlong/chatappprojectbackend/ChatAppProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgdlong.chatappprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
